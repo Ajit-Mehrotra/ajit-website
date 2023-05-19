13c0a21bc8add54124e81e53e4498788b1b4392f
Ajit <ajitmehrotra@gmail.com>
2023-05-18 21:21:37 -0400

    landing page draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     colors:{
+        'cremey': '#d9cca8',
+        'cremey-light': '#eae5da',
+        'val-red':'#da292a',
+     }
     },
   },
   plugins: [],
